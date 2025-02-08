a756e6304ffc307fa3dc85b2b4cf92edaed22dd1
Koruda <158458944+Crissdavi@users.noreply.github.com>
2025-02-08 11:42:42 -0500

    Actualizar _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -1,9 +1,36 @@
 import fetch from 'node-fetch'
-export async function before(m,{conn }) {
-	
-	let who = m.sender ? m.sender : conn.user.jid && conn.user.jid ? conn.user.jid : '0@s.whatsapp.net'
-	let pp = await this.profilePictureUrl(who, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg')
-	
-	//reply link wa
-   global.rpl = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: link_uni, mediaType: 'VIDEO', description: 'Grupo ofc de Simple Wa Bot', title: 'Simple Wa Bot | FzTeis', body: 'grupo de soporte', thumbnailUrl: 'https://telegra.ph/file/e0a75db0c8d1dd7b1cd27.jpg', sourceUrl: link_uni }}}
+
+export async function before(m, { conn }) {
+let img = await (await fetch(`https://i.ibb.co/W4PRfw3f/file.jpg`)).buffer()
+
+ global.rcanal = {
+    contextInfo: {
+            isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: "120363290084838811@newsletter",
+      serverMessageId: 100,
+      newsletterName: namechannel,
+    },
+    },
+  }
+
+ global.adReply = {
+            contextInfo: { 
+             forwardingScore: 9999, 
+                 isForwarded: false, 
+                    externalAdReply: {
+                                    showAdAttribution: true,
+                                        title: botname,
+                                        body: textbot,
+                                        mediaUrl: null,
+                                        description: null,
+                                        previewType: "PHOTO",
+                                        thumbnailUrl: img,
+                    thumbnail: img,
+                           sourceUrl: canal,
+                           mediaType: 1,
+                   renderLargerThumbnail: true
+                                }
+                        }
+                }
 }
